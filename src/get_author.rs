b65896cddec3a8875bcda1a7fde24f5991be77c8
Olivier FAURE <olivier-faure@millcomputing.com>
2023-07-05 15:32:18 +0200

    Add tests for author data

--- src/get_author.rs
+++ src/get_author.rs
@@ -4,21 +4,30 @@ use std::{collections::HashMap, path::Path};
 
 #[derive(Debug, Default, Deserialize)]
 pub struct AuthorData {
+    /// Maps author wiki-names to 'Full NAME <email@example.com>' strings
     pub authors: HashMap<String, String>,
-}
-
-#[derive(Debug, Default, Deserialize)]
-pub struct AuthorRecord {
-    pub name: String,
-    pub email: String,
 }
 
 pub fn load_author_data(filename: &Path) -> Result<AuthorData, csv::Error> {
     let mut reader = csv::Reader::from_path(filename)?;
     let mut authors: HashMap<String, String> = HashMap::new();
-    for result in reader.deserialize() {
-        let record: AuthorRecord = result?;
-        authors.insert(record.name, record.email);
+    for record in reader.into_records() {
+        let (name, email) = record?.deserialize(None)?;
+        authors.insert(name, email);
     }
     Ok(AuthorData { authors })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use insta::assert_debug_snapshot;
+
+    #[test]
+    fn test_load_author_data() {
+        let author_data = load_author_data(Path::new("test_files/example_names.csv")).unwrap();
+        let mut author_data = author_data.authors.into_iter().collect::<Vec<_>>();
+        author_data.sort();
+        assert_debug_snapshot!(author_data);
+    }
 }
