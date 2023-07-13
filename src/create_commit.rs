a3029959de31d7af896f9f00d1430fca604f2d44
Olivier FAURE <olivier-faure@millcomputing.com>
2023-07-13 14:41:14 +0200

    Assemble scaffholding

--- src/create_commit.rs
+++ src/create_commit.rs
@@ -39,24 +39,6 @@ pub fn create_commit_from_metadata(
     );
 }
 
-pub fn get_file_path(page_name: &str) -> PathBuf {
-    // replace spaces with underscores
-    let page_name = page_name.replace(" ", "_");
-    // skip forbidden characters
-    let page_name = page_name.replace(
-        &[
-            '<', '>', ':', '\'', '|', '?', '*', '\0', '\x01', '\x02', '\x03', '\x04', '\x05',
-            '\x06', '\x07', '\x08', '\x09', '\x0a', '\x0b', '\x0c', '\x0d', '\x0e', '\x0f', '\x10',
-            '\x11', '\x12', '\x13', '\x14', '\x15', '\x16', '\x17', '\x18', '\x19', '\x1a', '\x1b',
-            '\x1c', '\x1d', '\x1e', '\x1f',
-        ][..],
-        "",
-    );
-
-    // automatically handles path separators
-    PathBuf::from(page_name).with_extension("md")
-}
-
 pub fn strip_special_characters(page_name: &str) -> String {
     // replace spaces with underscores
     let page_name = page_name.replace(" ", "_");
--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,14 @@ mod fetch_revisions;
 mod get_author_data;
 
 use convert_file::convert_file;
-use create_commit::{create_commit_from_metadata, strip_special_characters};
-use git2::{Repository, Signature, Time};
+use create_commit::{
+    create_commit_from_metadata, get_signature, normalize_special_characters,
+    strip_special_characters,
+};
+use git2::{BranchType, Repository, Signature, Time};
 use reqwest::Error;
 use serde::Deserialize;
+use tokio::task::LocalSet;
 use tokio::{spawn, sync::mpsc, task::spawn_local};
 
 use std::collections::HashMap;
@@ -20,60 +24,119 @@ use std::path::{Path, PathBuf};
 
 use fetch_all_pages::fetch_all_pages;
 use fetch_revisions::{fetch_revisions, get_parsed_revisions, ParsedRevision};
-use get_author_data::{load_author_data, AuthorData};
-
-use crate::create_commit::{get_file_path, get_signature};
+use get_author_data::{load_author_data, Author, AuthorData};
 
 // TODO - skip redirections
 // TODO - handle talk and user pages
+// TODO - unwrap
 
-// 0 - Create repository
-// 1 - Fetch all pages
-// For each page:
-// 2 - Fetch all revisions
-// For each revision
-// - Write change content to file (create parents if necessary)
-// - Commit change to repository
+struct ProgramArgs {
+    wiki_url: String,
+    page_count: Option<u32>,
+    revision_count: Option<u32>,
+    strip_special_chars: bool,
+    output_dir: PathBuf,
+    author_data: Option<PathBuf>,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let (mut sender, mut receiver) = mpsc::channel(32); // Create an async channel
 
-    //let url_base = "";
-    let client = reqwest::Client::new();
-    let url = "https://yourwiki.com/w/api.php".to_string();
-    // TODO - unwrap
-    let author_data = load_author_data(Path::new("authors.csv")).unwrap();
-    let repo_path = Path::new("repo");
+    let program_args = ProgramArgs {
+        wiki_url: "https://wiki.archlinux.org".to_string(),
+        page_count: Some(5),
+        revision_count: Some(5),
+        strip_special_chars: true,
+        output_dir: PathBuf::from("output"),
+        author_data: None,
+    };
 
-    let mut repository = git2::Repository::open(repo_path).unwrap();
+    // TODO - handle case where user gives xxx/api.php
 
-    let producer = spawn(async move { task_get_revisions(&client, &url, &mut sender).await });
+    let wiki_url = program_args.wiki_url;
+    let url = format!("{wiki_url}/api.php");
+    let author_data = if let Some(author_data_path) = program_args.author_data {
+        load_author_data(&author_data_path).unwrap()
+    } else {
+        AuthorData::default()
+    };
 
-    let consumer = spawn_local(async move {
-        task_process_revisions(&author_data, &mut receiver, &mut repository).await;
-    });
+    // If path exists, open repository, else create new repository
+    let mut repository = if program_args.output_dir.exists() {
+        Repository::open(&program_args.output_dir).unwrap()
+    } else {
+        let committer = Signature::new("wiki2git", "wiki2git", &Time::new(0, 0)).unwrap();
+        create_repo::create_repo(&program_args.output_dir.to_str().unwrap(), committer).unwrap()
+    };
+    git2::Repository::open(&program_args.output_dir).unwrap();
 
-    tokio::try_join!(producer, consumer).unwrap();
+    // Set of thread-local tasks (which, given Repostiory is not Send, is everything)
+    let local_set = LocalSet::new();
+    local_set
+        .run_until(async move {
+            let producer = spawn(async move {
+                task_get_revisions(
+                    &url,
+                    &mut sender,
+                    program_args.page_count,
+                    program_args.revision_count,
+                )
+                .await
+            });
+
+            let consumer = spawn_local(async move {
+                task_process_revisions(
+                    &author_data,
+                    &mut receiver,
+                    &mut repository,
+                    &program_args.output_dir,
+                    program_args.strip_special_chars,
+                )
+                .await;
+            });
+
+            tokio::try_join!(producer, consumer).unwrap();
+        })
+        .await;
 
     Ok(())
 }
 
 async fn task_get_revisions(
-    client: &reqwest::Client,
     url: &str,
     sender: &mut mpsc::Sender<ParsedRevision>,
+    page_count: Option<u32>,
+    revision_count: Option<u32>,
 ) -> Result<(), Error> {
+    let client = reqwest::Client::new();
+
+    let mut page_count = page_count;
+    let mut revision_count = revision_count;
     loop {
+        if let Some(ref mut count) = page_count {
+            if *count == 0 {
+                break;
+            }
+            (*count) -= 1;
+        }
+
         let mut ap_continue_token = None;
-        let mut pages = fetch_all_pages(client, url, None, ap_continue_token).await?;
+        let mut pages = fetch_all_pages(&client, url, None, ap_continue_token).await?;
 
         for page in &pages.query.allpages {
             let pageid = page.pageid;
             let mut rv_continue_token = None;
             loop {
+                if let Some(ref mut count) = revision_count {
+                    if *count == 0 {
+                        break;
+                    }
+                    (*count) -= 1;
+                }
+
                 let mut revisions =
-                    fetch_revisions(client, url, pageid, None, rv_continue_token).await?;
+                    fetch_revisions(&client, url, pageid, None, rv_continue_token).await?;
 
                 for revision in get_parsed_revisions(revisions.query, page.title.clone().into()) {
                     sender.send(revision).await.unwrap();
@@ -98,10 +161,31 @@ async fn task_process_revisions(
     author_data: &AuthorData,
     receiver: &mut mpsc::Receiver<ParsedRevision>,
     repository: &mut Repository,
+    repository_path: &Path,
+    strip_special_chars: bool,
 ) -> Result<(), std::io::Error> {
     let authors = &author_data.authors;
     while let Some(revision) = receiver.recv().await {
-        let file_path = get_file_path(&revision.title);
+        let normalized_title = if strip_special_chars {
+            strip_special_characters(&revision.title)
+        } else {
+            normalize_special_characters(&revision.title)
+        };
+        let file_path = Path::new(&normalized_title).with_extension("md");
+
+        // add new branch to repository if doesn't exist
+        if repository
+            .find_branch(&normalized_title, BranchType::Local)
+            .is_err()
+        {
+            repository
+                .branch(
+                    &normalized_title,
+                    &repository.head().unwrap().peel_to_commit().unwrap(),
+                    false,
+                )
+                .unwrap();
+        }
 
         // create parent directories if necessary
         if let Some(parent) = file_path.parent() {
@@ -111,22 +195,26 @@ async fn task_process_revisions(
         // execute pandoc command with revision.content as input and write to file_path
         let title = revision.title.clone();
         let content = revision.content.clone();
+        let absolute_file_path = repository_path.join(&file_path);
         spawn(async move {
-            convert_file(&file_path, &title, &content);
+            convert_file(&absolute_file_path, &title, &content);
         })
         .await;
 
-        let author_git_data = authors.get(&revision.user).unwrap();
+        // FIXME - implement more sensible default
+        let default_author = Author {
+            name: "Unknown author".to_string(),
+            email: "unknown-email@example.com".to_string(),
+        };
+        let author_git_data = authors.get(&revision.user).unwrap_or(&default_author);
         let mut author = get_signature(&revision, &author_git_data);
         let mut committer = Signature::new("name", "email", &Time::new(0, 0)).unwrap();
 
-        let branch_name = strip_special_characters(&revision.title);
-        let file_path = get_file_path(&revision.title);
         create_commit_from_metadata(
             repository,
             committer,
             author,
-            &branch_name,
+            &normalized_title,
             &file_path,
             &revision.comment,
         );
