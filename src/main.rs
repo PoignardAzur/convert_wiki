2b81b275b616d3aaca0d8691f35339a7c8fa42df
Olivier FAURE <olivier-faure@millcomputing.com>
2023-07-13 17:17:46 +0200

    Reorganize main loop architecture

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ use std::collections::HashMap;
 use std::io::Write;
 use std::path::{Path, PathBuf};
 
-use fetch_all_pages::fetch_all_pages;
+use fetch_all_pages::{fetch_all_pages, Page};
 use fetch_revisions::{fetch_revisions, get_parsed_revisions, ParsedRevision};
 use get_author_data::{load_author_data, Author, AuthorData};
 
@@ -40,8 +40,6 @@ struct ProgramArgs {
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let (mut sender, mut receiver) = mpsc::channel(32); // Create an async channel
-
     let program_args = ProgramArgs {
         wiki_url: "https://wiki.archlinux.org".to_string(),
         page_count: Some(5),
@@ -61,6 +59,8 @@ async fn main() -> Result<(), Error> {
         AuthorData::default()
     };
 
+    let client = reqwest::Client::new();
+
     // If path exists, open repository, else create new repository
     let mut repository = if program_args.output_dir.exists() {
         Repository::open(&program_args.output_dir).unwrap()
@@ -70,82 +70,78 @@ async fn main() -> Result<(), Error> {
     };
     git2::Repository::open(&program_args.output_dir).unwrap();
 
-    // Set of thread-local tasks (which, given Repostiory is not Send, is everything)
-    let local_set = LocalSet::new();
-    local_set
-        .run_until(async move {
-            let producer = spawn(async move {
-                task_get_revisions(
-                    &url,
-                    &mut sender,
-                    program_args.page_count,
-                    program_args.revision_count,
-                )
-                .await
-            });
+    let (mut page_sender, mut page_receiver) = mpsc::channel(8); // Create an async channel
+    let (mut rev_sender, mut rev_receiver) = mpsc::channel(32); // Create an async channel
 
-            let consumer = spawn_local(async move {
-                task_process_revisions(
+    // Represents a set of task run on the main thread
+    let local_set = LocalSet::new();
+
+    // Set of thread-local tasks (which, given Repository is not Send, is everything)
+    let client_clone = client.clone();
+    let url_clone = url.clone();
+    let pages_task = spawn(async move {
+        task_get_pages(
+            &client_clone,
+            &url_clone,
+            &mut page_sender,
+            program_args.page_count,
+        )
+        .await
+    });
+
+    let revs_task = spawn(async move {
+        let mut revision_count = program_args.revision_count;
+        while let Some(page) = page_receiver.recv().await {
+            task_get_revisions(
+                &client,
+                &url,
+                page,
+                &mut rev_sender,
+                program_args.revision_count,
+            )
+            .await;
+        }
+    });
+
+    let commit_task = local_set.run_until(async move {
+        spawn_local(async move {
+            while let Some(revision) = rev_receiver.recv().await {
+                task_process_revision(
                     &author_data,
-                    &mut receiver,
+                    revision,
                     &mut repository,
                     &program_args.output_dir,
                     program_args.strip_special_chars,
                 )
                 .await;
-            });
-
-            tokio::try_join!(producer, consumer).unwrap();
+            }
         })
-        .await;
+        .await
+    });
+
+    tokio::try_join!(pages_task, revs_task, commit_task).unwrap();
 
     Ok(())
 }
 
-async fn task_get_revisions(
+async fn task_get_pages(
+    client: &reqwest::Client,
     url: &str,
-    sender: &mut mpsc::Sender<ParsedRevision>,
+    sender: &mut mpsc::Sender<Page>,
     page_count: Option<u32>,
-    revision_count: Option<u32>,
 ) -> Result<(), Error> {
-    let client = reqwest::Client::new();
-
     let mut page_count = page_count;
-    let mut revision_count = revision_count;
     loop {
-        if let Some(ref mut count) = page_count {
-            if *count == 0 {
-                break;
-            }
-            (*count) -= 1;
-        }
-
         let mut ap_continue_token = None;
         let mut pages = fetch_all_pages(&client, url, None, ap_continue_token).await?;
 
-        for page in &pages.query.allpages {
-            let pageid = page.pageid;
-            let mut rv_continue_token = None;
-            loop {
-                if let Some(ref mut count) = revision_count {
-                    if *count == 0 {
-                        break;
-                    }
-                    (*count) -= 1;
-                }
-
-                let mut revisions =
-                    fetch_revisions(&client, url, pageid, None, rv_continue_token).await?;
-
-                for revision in get_parsed_revisions(revisions.query, page.title.clone().into()) {
-                    sender.send(revision).await.unwrap();
-                }
-
-                rv_continue_token = revisions.cont;
-                if rv_continue_token.is_none() {
-                    break;
-                }
+        for page in pages.query.allpages {
+            if let Some(0) = page_count {
+                return Ok(());
             }
+            page_count = page_count.map(|count| count - 1);
+
+            sender.send(page).await.unwrap();
         }
 
         ap_continue_token = pages.cont;
@@ -156,65 +152,95 @@ async fn task_get_revisions(
     Ok(())
 }
 
-async fn task_process_revisions(
+async fn task_get_revisions(
+    client: &reqwest::Client,
+    url: &str,
+    page: Page,
+    sender: &mut mpsc::Sender<ParsedRevision>,
+    revision_count: Option<u32>,
+) -> Result<(), Error> {
+    let pageid = page.pageid;
+    let mut revision_count = revision_count;
+    let mut rv_continue_token = None;
+    loop {
+        println!("Fetching revisions for page {}", page.title);
+
+        let mut revisions = fetch_revisions(&client, url, pageid, None, rv_continue_token).await?;
+
+        for revision in get_parsed_revisions(revisions.query, page.title.clone().into()) {
+            if let Some(0) = revision_count {
+                break;
+            }
+            revision_count = revision_count.map(|count| count - 1);
+
+            sender.send(revision).await.unwrap();
+        }
+
+        rv_continue_token = revisions.cont;
+        if rv_continue_token.is_none() {
+            break;
+        }
+    }
+    Ok(())
+}
+
+async fn task_process_revision(
     author_data: &AuthorData,
-    receiver: &mut mpsc::Receiver<ParsedRevision>,
+    revision: ParsedRevision,
     repository: &mut Repository,
     repository_path: &Path,
     strip_special_chars: bool,
 ) -> Result<(), std::io::Error> {
     let authors = &author_data.authors;
-    while let Some(revision) = receiver.recv().await {
-        // TODO - strip_special_chars
-        let file_path = Path::new(&get_file_name(&revision.title)).with_extension("md");
-        let branch_name = get_branch_name(&revision.title);
 
-        // add new branch to repository if doesn't exist
-        if repository
-            .find_branch(&branch_name, BranchType::Local)
-            .is_err()
-        {
-            repository
-                .branch(
-                    &branch_name,
-                    &repository.head().unwrap().peel_to_commit().unwrap(),
-                    false,
-                )
-                .unwrap();
-        }
+    let file_path = Path::new(&get_file_name(&revision.title)).with_extension("md");
+    let branch_name = get_branch_name(&revision.title);
 
-        // create parent directories if necessary
-        if let Some(parent) = file_path.parent() {
-            tokio::fs::create_dir_all(parent).await?;
-        }
-
-        // execute pandoc command with revision.content as input and write to file_path
-        let title = revision.title.clone();
-        let content = revision.content.clone();
-        let absolute_file_path = repository_path.join(&file_path);
-        spawn(async move {
-            convert_file(&absolute_file_path, &title, &content);
-        })
-        .await;
-
-        // FIXME - implement more sensible default
-        let default_author = Author {
-            name: "Unknown author".to_string(),
-            email: "unknown-email@example.com".to_string(),
-        };
-        let author_git_data = authors.get(&revision.user).unwrap_or(&default_author);
-        let mut author = get_signature(&revision, &author_git_data);
-        let mut committer = Signature::new("name", "email", &Time::new(0, 0)).unwrap();
-
-        create_commit_from_metadata(
-            repository,
-            committer,
-            author,
-            &branch_name,
-            &file_path,
-            &revision.comment,
-        );
+    // add new branch to repository if doesn't exist
+    if repository
+        .find_branch(&branch_name, BranchType::Local)
+        .is_err()
+    {
+        repository
+            .branch(
+                &branch_name,
+                &repository.head().unwrap().peel_to_commit().unwrap(),
+                false,
+            )
+            .unwrap();
     }
+
+    // create parent directories if necessary
+    if let Some(parent) = file_path.parent() {
+        tokio::fs::create_dir_all(parent).await?;
+    }
+
+    // execute pandoc command with revision.content as input and write to file_path
+    let title = revision.title.clone();
+    let content = revision.content.clone();
+    let absolute_file_path = repository_path.join(&file_path);
+    spawn(async move {
+        convert_file(&absolute_file_path, &title, &content);
+    })
+    .await;
+
+    // FIXME - implement more sensible default
+    let default_author = Author {
+        name: "Unknown author".to_string(),
+        email: "unknown-email@example.com".to_string(),
+    };
+    let author_git_data = authors.get(&revision.user).unwrap_or(&default_author);
+    let mut author = get_signature(&revision, &author_git_data);
+    let mut committer = Signature::new("name", "email", &Time::new(0, 0)).unwrap();
+
+    create_commit_from_metadata(
+        repository,
+        committer,
+        author,
+        &branch_name,
+        &file_path,
+        &revision.comment,
+    );
 
     Ok(())
 }
